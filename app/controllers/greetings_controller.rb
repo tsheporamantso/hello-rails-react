5e2564cee29b845014248c659d67683b82447639
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-20 16:15:25 +0200

    Define random action that collects randoms greeting from Message table and render json

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,7 @@
 class GreetingsController < ApplicationController
+
+  def random
+    random_message = Message.order('RANDOM()').first
+    render json: { greeting: random_message&.text }
+  end
 end
